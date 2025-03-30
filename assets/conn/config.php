80f4b1d0fd90a9dff64ec340fdda9ddfcd86616a
Lasse Lykke <mail@lasselykke.com>
2025-03-30 15:32:07 +0200

    edit filestructure

---
+++ assets/conn/config.php
@@ -0,0 +1,37 @@
+<?php
+include 'conn.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="refresh" content="1500;url=../conn/logout.php" />
+    <!--Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=DM+Serif+Text&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Poppins&display=swap"
+        rel="stylesheet">
+
+    <!--sub folders-->
+    <link href="../assets/style/base.css" type="text/css" rel="stylesheet">
+    <link href="../assets/style/layout.css" type="text/css" rel="stylesheet">
+    <link href="../assets/style/comp.css" type="text/css" rel="stylesheet">
+
+    <!-- Sub folders to backend -->
+
+
+
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <meta name="author" content="Lasse Lykke @ Lasselykke.com" />
+
+    <!-- JS SCRIPT's -->
+    <script src="../assets/style/script.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script> <!-- Chart.js inkludering -->
+    <script src="https://cdn.jsdelivr.net/npm/moment@2.29.4/moment.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chartjs-adapter-moment@1.0.0"></script>
+</head>
---
+++ index.php
@@ -0,0 +1,24 @@
+<?php 
+include './assets/conn/config.php';
+include 'assets/include/nav.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title>Lasse Lykke </title>
+</head>
+
+<body>
+    
+    </div>
+
+</body>
+<script src="../assets/style/script.js"></script>
+
+</html>
