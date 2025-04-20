06d43ba2b8c174fbb8ba29a9347c608806a1e36e
Lasse Lykke <mail@lasselykke.com>
2025-04-20 15:04:04 +0200

    finish project overview site. Added grid, and btn styling.

--- projects.php
+++ projects.php
@@ -20,25 +20,38 @@ $projects = $result->fetch_all(MYSQLI_ASSOC);
 
 <body>
     <div class="contentWrapper">
-    <section class="breadcrumbs">
-        <p>projects / <span>all</span></p>
-    </section>
-    <div class="wrapper">
+        <section class="breadcrumbs">
+            <p>projects / <span>all</span></p>
+        </section>
+        <div class="wrapper">
 
-        <div class="allProjectWrapper">
+            <div class="allProjectWrapper">
 
-            <?php foreach ($projects as $project): ?>
-                <div class="project-card">
-                    <h2><?php echo htmlspecialchars($project['title']); ?></h2>
-                    <p><strong>Tech used:</strong> <?php echo htmlspecialchars($project['tags']); ?></p>
-                    <p><?php echo nl2br(htmlspecialchars($project['short_description'])); ?></p><br>
-                    <a href="singleProject.php?id=<?php echo $project['id']; ?>" class="read-more">Read More</a>
+                <?php foreach ($projects as $project): ?>
+                    <?php
+                    // Hent det første billede for hvert projekt
+                    $sql_image = "SELECT image_path FROM project_images WHERE project_id = ? ORDER BY uploaded_at ASC LIMIT 1";
+                    $stmt_image = $conn->prepare($sql_image);
+                    $stmt_image->bind_param('i', $project['id']);
+                    $stmt_image->execute();
+                    $result_image = $stmt_image->get_result();
+                    $first_image = $result_image->fetch_assoc();
+                    ?>
 
-                </div>
-            <?php endforeach; ?>
+                    <div class="project-card">
+                        <h2><?php echo htmlspecialchars($project['title']); ?></h2>
+                        <p><strong>Tech used:</strong> <?php echo htmlspecialchars($project['tags']); ?></p>
+                        <?php if ($first_image): ?>
+                            <img src="<?php echo htmlspecialchars($first_image['image_path']); ?>"
+                                alt="CoverImages <?php echo htmlspecialchars($project['title']); ?>" class="project-image">
+                        <?php endif; ?>
+                        <p><?php echo nl2br(htmlspecialchars($project['short_description'])); ?></p>
+                        <a href="singleProject.php?id=<?php echo $project['id']; ?>" class="read-more">Read More</a>
+                    </div>
+                <?php endforeach; ?>
+            </div>
         </div>
     </div>
-            </div>
 </body>
 <script src="./assets/style/script.js"></script>
 
