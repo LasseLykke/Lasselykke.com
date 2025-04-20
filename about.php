59558f5c47f8d98e9529bd08fc9522282ebcc913
Lasse Lykke <mail@lasselykke.com>
2025-04-20 20:08:51 +0200

    working on about. Thinking of merging about and contact together, but not sure yet.

---
+++ about.php
@@ -0,0 +1,55 @@
+<?php
+include './assets/conn/config.php';
+include './assets/include/nav.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>About - Lasse Lykke</title>
+</head>
+
+<body>
+    <div class="contentWrapper">
+        <section class="breadcrumbs">
+            <p>about / <span>Lasse</span></p>
+        </section>
+        <div class="wrapper">
+
+            <div class="aboutWrapper">
+                <section class="aboutContent">
+                    <h1>About</h1>
+                    <p>Hi there! I’m a self-taught web developer with a passion for bringing data to life through clean
+                        code
+                        and thoughtful design. <br>
+                        I work primarily with HTML, CSS/SASS, PHP, and JavaScript—and I’m always curious to explore new
+                        languages and technologies when inspiration strikes.<br>
+                        My skillset also includes working with SQL and databases, which I enjoy shaping into efficient,
+                        reliable backends for dynamic websites and tools.<br>
+                        Whether it's crafting a custom CMS, visualizing user data, or building interactive interfaces, I
+                        aim
+                        to create digital solutions that just make sense.<br>
+                        I like to think of myself as a friendly hacker—the kind who solves everyday challenges with
+                        clever
+                        tech, not the kind in a hoodie breaking into your WiFi. Coding may not be my full-time job, but
+                        it’s
+                        definitely my favorite way to unwind and think creatively.</p>
+                </section>
+                <section class="aboutContent">
+                    <img src="./uploads/about-1.png" class="aboutImage">
+                </section>
+
+                <h1>Contact</h1>
+
+
+            </div>
+        </div>
+    </div>
+</body>
+<script src="./assets/style/script.js"></script>
+
+</html>
